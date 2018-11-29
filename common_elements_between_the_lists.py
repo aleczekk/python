836d79022057794a7bf879861463140ff14caf8a
aleczekk <45439548+aleczekk@users.noreply.github.com>
2018-11-29 22:35:57 +0100

    Update common_elements_between_the_lists.py

--- common_elements_between_the_lists.py
+++ common_elements_between_the_lists.py
@@ -1,9 +1,13 @@
 import random  # import the random module to use function
 
-a = random.sample(range(30), 5)  # generates random list in range 0 to 30
+#a = random.sample(range(30), 5)  # generates random list in range 0 to 30
+a = [0, 1, 3, 3, 4, 4, 4]
 b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = []
-for element in b:
-    if element in a:
-        c.append(element)  # add element to list after if
+for element in a:
+    if element in b:
+        if element in c:  # prevents number repeat
+            print('duplicates are ', element)
+        else:
+            c.append(element)  # add element to list after if
 print(c)
